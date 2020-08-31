7b7713eb294660537bf91e16201f4dacb6db8004
Kuruba Mahender <mahekvs567@gmail.com>
2020-08-31 20:06:36 +0530

    ConcurrentStack

---
+++ Tasks/ConcurrentCollections/ConcurrentStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tasks
+{
+    internal class ConcurrentStack
+    {
+        public void MainCall()
+        {
+            var stack = new ConcurrentStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            int result;
+            if (stack.TryPeek(out result))
+                Console.WriteLine($"{result} is on top");
+
+            if (stack.TryPop(out result))
+                Console.WriteLine($"Popped {result}");
+
+            var items = new int[5];
+            if (stack.TryPopRange(items, 0, 5) > 0) // actually pops only 3 items
+            {
+                var text = string.Join(", ", items.Select(i => i.ToString()));
+                Console.WriteLine($"Popped these items: {text}");
+                // OutPut: 3,2,1,0,0
+            }
+
+        }
+    }
+}
