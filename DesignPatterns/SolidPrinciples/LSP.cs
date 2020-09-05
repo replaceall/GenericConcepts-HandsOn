d1986798d02eba374606f735706e554e9fba18c1
Kuruba Mahender <mahekvs567@gmail.com>
2020-09-05 20:37:40 +0530

    Design Patterns | SRP | OCP

---
+++ DesignPatterns/SolidPrinciples/LSP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace DesignPatterns.SolidPrinciples
+{
+    internal class LSP
+    {
+
+        public void MainCall()
+        {
+        }
+    }
+}
