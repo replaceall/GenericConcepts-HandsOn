b0964688b74bc7dc4d65c4f014af2700542511bc
Kuruba Mahender <>
2020-11-27 13:01:02 +0530

    Spin -locking

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPatterns;
+using System;
 
 namespace Tasks
 {
@@ -7,6 +8,35 @@ namespace Tasks
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
         }
+
+        /*
+         *  Task Vs Thread
+         *  https://stackoverflow.com/questions/4130194/what-is-the-difference-between-task-and-thread
+         * */
+
+        /*
+         * Atomicity
+         * 
+         */
+
+        /*
+         * Critical Sections:
+         * Lock object is useful to access the given Property by single thread only for a given time.
+         */
+
+        /*
+         * InterLocked: (another approach for Critical sections , but not preferred all the times.)
+         * InterLocked.Add(__, ); for Adding
+         * InterLocked.Add(__, -x); for Substraction
+         * 
+         * InterLocked.MemoryBarrior(__, ..); https://stackoverflow.com/questions/3493931/why-do-i-need-a-memory-barrier
+         * 
+         */
+
+        /* 
+         * ConcurrentBag, ConcurrentStack, ConcurrentQueue are treated as  IProducerConsumerCollection
+         */
     }
 }
